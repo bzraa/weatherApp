2b5f31794b71e2491a057a5a49532e73d1ac5f74
Bazarsad Uurdmandakh <ubazraa3@gmail.com>
2023-03-06 02:28:56 +0800

    6th

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import CityComponent from "./modules/CityComponent";
 import WeatherComponent from "./modules/WeatherInfoComponent";
 
 export const WeatherIcons = {
-  "01d": "/react-weather-app/icons/sunny.svg",
-  "01n": "/react-weather-app/icons/night.svg",
-  "02d": "/react-weather-app/icons/day.svg",
-  "02n": "/react-weather-app/icons/cloudy-night.svg",
-  "03d": "/react-weather-app/icons/cloudy.svg",
-  "03n": "/react-weather-app/icons/cloudy.svg",
-  "04d": "/react-weather-app/icons/perfect-day.svg",
-  "04n": "/react-weather-app/icons/cloudy-night.svg",
-  "09d": "/react-weather-app/icons/rain.svg",
-  "09n": "/react-weather-app/icons/rain-night.svg",
-  "10d": "/react-weather-app/icons/rain.svg",
-  "10n": "/react-weather-app/icons/rain-night.svg",
-  "11d": "/react-weather-app/icons/storm.svg",
-  "11n": "/react-weather-app/icons/storm.svg",
+  "01d": "/react-weather-app/src/icons/sunny.svg",
+  "01n": "/react-weather-app/src/icons/night.svg",
+  "02d": "/react-weather-app/src/icons/day.svg",
+  "02n": "/react-weather-app/src/icons/cloudy-night.svg",
+  "03d": "/react-weather-app/src/icons/cloudy.svg",
+  "03n": "/react-weather-app/src/icons/cloudy.svg",
+  "04d": "/react-weather-app/src/icons/perfect-day.svg",
+  "04n": "/react-weather-app/src/icons/cloudy-night.svg",
+  "09d": "/react-weather-app/src/icons/rain.svg",
+  "09n": "/react-weather-app/src/icons/rain-night.svg",
+  "10d": "/react-weather-app/src/icons/rain.svg",
+  "10n": "/react-weather-app/src/icons/rain-night.svg",
+  "11d": "/react-weather-app/src/icons/storm.svg",
+  "11n": "/react-weather-app/src/icons/storm.svg",
 };
 
 const Container = styled.div`
