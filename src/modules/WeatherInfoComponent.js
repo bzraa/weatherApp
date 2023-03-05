9a5d54e14950b54ad1804cb55607c5d3c791d70d
Bazarsad Uurdmandakh <ubazraa3@gmail.com>
2023-03-06 01:59:46 +0800

    second

---
+++ src/modules/WeatherInfoComponent.js
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components";
+import {WeatherIcons} from "../App";
+
+export const WeatherInfoIcons = {
+    Жаргах: "/react-weather-app/icons/temp.svg",
+    Мандах: "/react-weather-app/icons/temp.svg",
+    Чийг: "/react-weather-app/icons/humidity.svg",
+    Салхи: "/react-weather-app/icons/wind.svg",
+    Даралт: "/react-weather-app/icons/pressure.svg",
+};
+const Location = styled.span`
+  margin: 15px auto;
+  text-transform: capitalize;
+  font-size: 28px;
+  font-weight: bold;
+`;
+const Condition = styled.span`
+  margin: 20px auto;
+  text-transform: capitalize;
+  font-size: 14px;
+  & span {
+    font-size: 28px;
+  }
+`;
+const WeatherInfoLabel = styled.span`
+  margin: 20px 25px 10px;
+  text-transform: capitalize;
+  text-align: start;
+  width: 90%;
+  font-weight: bold;
+  font-size: 14px;
+`;
+const WeatherIcon = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 5px auto;
+`;
+const WeatherContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 30px auto;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const WeatherInfoContainer = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+const InfoContainer = styled.div`
+  display: flex;
+  margin: 5px 10px;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+const InfoIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+const InfoLabel = styled.span`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  margin: 15px;
+  & span {
+    font-size: 12px;
+    text-transform: capitalize;
+  }
+`;
+
+const WeatherInfoComponent = (props) => {
+    const {name, value} = props;
+    return (
+        <InfoContainer>
+            <InfoIcon src={WeatherInfoIcons[name]}/>
+            <InfoLabel>
+                {value}
+                <span>{name}</span>
+            </InfoLabel>
+        </InfoContainer>
+    );
+};
+const WeatherComponent = (props) => {
+    const {weather} = props;
+    const isDay = weather?.weather[0].icon?.includes('d')
+    const getTime = (timeStamp) => {
+        return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
+    }
+    return (
+        <>
+            <WeatherContainer>
+                <Condition>
+                    <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span>
+                    {`  |  ${weather?.weather[0].description}`}
+                </Condition>
+                <WeatherIcon src={WeatherIcons[weather?.weather[0].icon]}/>
+            </WeatherContainer>
+            <Location>{`${weather?.name}, ${weather?.sys?.country}`}</Location>
+
+            <WeatherInfoLabel>Цаг агаарын мэдээ</WeatherInfoLabel>
+            <WeatherInfoContainer>
+                <WeatherInfoComponent name={isDay ? "Жаргах" : "Мандах"}
+                                      value={`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`}/>
+                <WeatherInfoComponent name={"Чийг"} value={weather?.main?.humidity}/>
+                <WeatherInfoComponent name={"Салхи"} value={weather?.wind?.speed}/>
+                <WeatherInfoComponent name={"Даралт"} value={weather?.main?.pressure}/>
+            </WeatherInfoContainer>
+        </>
+    );
+};
+
+export default WeatherComponent;
